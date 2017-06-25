d42387077bff314b06c27c81f91b8b6be1e33d9b
Diego Costantino <diegocstn@gmail.com>
2017-06-25 14:09:52 +0200

    check division by zero

--- jlox/src/com/craftinginterpreters/lox/Interpreter.java
+++ jlox/src/com/craftinginterpreters/lox/Interpreter.java
@@ -87,6 +87,9 @@ public class Interpreter implements Expr.Visitor<Object>{
 
             case SLASH:
                 checkNumberOperands(expr.operator, left, right);
+                if ((double)right == 0) {
+                    throw new RuntimeError(expr.operator, "Division by 0 is not allowed.");
+                }
                 return (double) left / (double) right;
         }
 
